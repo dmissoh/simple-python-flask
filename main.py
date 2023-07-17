d4431d4f43ca5c8953391bc0d905b68d329a50a8
Dimitri Missoh <dimitri.missoh@maibornwolff.de>
2023-07-17 16:52:32 +0200

    change output

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Web App with Python Flask!'
+    return 'Dimitris First Web App with Python Flask!'
 
 app.run(host='0.0.0.0', port=81)
 
